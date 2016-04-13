0cad309a27941174489c5a807ba569a9b4a86e3c
Vladimir <scorpcgart@gmail.com>
2016-04-13 19:13:07 +0500

    что то добавил

--- gl6/index.php
+++ gl6/index.php
@@ -1,3 +1,14 @@
-<?php
-echo "PHP!!!! and MySQL!!!";
- ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Пример страницы (Глава 6)</title>
+</head>
+<body>
+  <p id="main">
+    Эта страница использует статические включениея для вывода внизу стандартного предупреждения об авторских правах.
+  </p>
+  <?php include 'footer.php'; ?>
+
+</body>
+</html>
