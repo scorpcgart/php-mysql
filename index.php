0e06c13186333af29c73414cefa8fabbe2d1822a
Vladimir <scorpcgart@gmail.com>
2016-04-13 17:11:16 +0500

    index.php и gl6

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Практические задание по книге PHP и MySQL</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <ul>
+      <li><a href="http://srv67381.ht-test.ru/php-mysql/gl6"> Глава 6 - Структурное программирование</a></li>
+      <li>Глава 7 - </li>
+      <li>Глава 8 -</li>
+      <li>Глава 9 -</li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </body>
+</html>
